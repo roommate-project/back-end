761ff2a94bd8ca15ac38d3cf0ea35a9d16df4042
im-shung <seoyoung1113@gmail.com>
2022-10-11 14:39:14 +0900

    FEAT : 웹 소켓 timeout 설정

--- src/main/java/roommateproject/roommatebackend/config/WebSocketConfig.java
+++ src/main/java/roommateproject/roommatebackend/config/WebSocketConfig.java
@@ -5,20 +5,38 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * Configure message broker.
+     * @param config
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
     }
 
+    /**
+     * Register stomp endpoints.
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    /**
+     * Configure web socket transport.
+     * @param registration
+     */
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(20 * 10000);
     }
 }
